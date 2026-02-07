f64ade9e38dac958519bdc7e32d9c41b22d92030
Osita Nwajah <101451314+oceeta@users.noreply.github.com>
2026-02-07 18:25:01 +0100

    Add functionality to buttons (except ".")
     - Technically, it works, but the calculator currently allows a user to
       enter more than one decimal point.
    
     - It is also worth noting that this commit has no way of allowing the
       input of a negative first number.

--- scripts/script.js
+++ scripts/script.js
@@ -39,19 +39,74 @@ function divide(a, b) {
 }
 
 buttons.addEventListener("click", function(e) {
-    if (!(e.target.classList[0] === "buttons" ||
-        e.target.classList[0] === "clear-buttons" ||
-        e.target.classList[0] === "nums-and-ops" ||
-        e.target.classList[0] === "button-row" ||
-        e.target.textContent === "/" ||
-        e.target.textContent === "x" ||
-        e.target.textContent === "-" ||
-        e.target.textContent === "+" ||
-        e.target.textContent === "=" ||
-        e.target.textContent === "AC" ||
-        e.target.textContent === "DEL")
-    ) {
+    const isButtonContainer = e.target.classList[0] === "buttons" || 
+                                e.target.classList[0] === "clear-buttons" || 
+                                e.target.classList[0] === "nums-and-ops" || 
+                                e.target.classList[0] === "button-row";
+
+    const isOperator = e.target.textContent === "/" ||
+                        e.target.textContent === "x" ||
+                        e.target.textContent === "-" ||
+                        e.target.textContent === "+";
+
+    const isEqualsTo = e.target.textContent === "=";
+
+    const isAllClear = e.target.textContent === "AC";
+    
+    const isDEL = e.target.textContent === "DEL";
+
+    const isNumber = !(isButtonContainer || isOperator || isEqualsTo || isAllClear || isDEL);
+
+    if (isNumber && operator === "") {
         num1 += e.target.textContent;
+        display.value = num1;
     }
-    display.value = num1;
+
+    if (num1 !== "" && operator !== "" && num2 !== "") {
+        if (isOperator) {
+            display.value = "";
+            num1 = parseFloat(num1);
+            num2 = parseFloat(num2);
+            display.value = operate(operator, num1, num2);
+            num1 = display.value;
+            operator = e.target.textContent;
+            num2 = "";
+        } else if (isEqualsTo) {
+            display.value = "";
+            num1 = parseFloat(num1);
+            num2 = parseFloat(num2);
+            display.value = operate(operator, num1, num2);
+            num1 = "";
+            operator = "";
+            num2 = "";
+        }
+    }
+
+    if (isOperator && num1 !== "" ) {
+        operator = e.target.textContent;
+    }
+
+    if (isNumber && num1 !== "" && operator !== "") {
+        display.value = "";
+        num2 += e.target.textContent;
+        display.value = num2;
+    }
+
+    // Clear display and "backspace"
+    if (isAllClear) {
+        num1 = "";
+        num2 = "";
+        operator = "";
+        display.value = "";
+    } else if (isDEL && operator !== "") {
+        let num2ToArray = num2.split("");
+        num2ToArray.pop();
+        num2 = num2ToArray.join("");
+        display.value = num2;
+    } else if (isDEL && operator === "") {
+        let num1ToArray = num1.split("");
+        num1ToArray.pop();
+        num1 = num1ToArray.join("");
+        display.value = num1;
+    }
 });
